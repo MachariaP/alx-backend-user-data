394ca41ae80c00eea78512e170c65e9b4da87f0e
MachariaP <walburphinehas78@gmail.com>
2024-08-01 09:25:41 +0300

    Correction 0f Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
-"""Create logger
+"""Connect to secure database
 """
 
+# filtered_logger.py
 import logging
 import re
+import os
+import mysql.connector
 from typing import List, Tuple
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
     """
     Obfuscate specified fields in a log message.
 
@@ -21,7 +25,7 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     Returns:
         str: The obfuscated log message.
     """
-    pattern = f"({'|'.join(fields)})=[^{separator}]*"
+    pattern = f"({'|'.join(fields)})=[^{re.escape(separator)}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
 
 
@@ -39,7 +43,8 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         original_message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        return filter_datum(self.fields,
+                            self.REDACTION, original_message, self.SEPARATOR)
 
 
 # Define PII_FIELDS
@@ -48,7 +53,8 @@ PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
 
 def get_logger() -> logging.Logger:
     """
-    Creates and returns a logger named "user_data" with specific configurations.
+    Creates and returns a logger named "user_data"
+    with specific configurations.
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
@@ -61,3 +67,20 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Connects to a secure database and returns the connection object.
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
