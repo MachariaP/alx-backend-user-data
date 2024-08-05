572bb81092e26a3de2023b74a70997c3d2801d6f
MachariaP <walburphinehas78@gmail.com>
2024-08-05 15:03:28 +0300

    Routes that don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,13 +5,20 @@
 from flask import request
 from typing import List, TypeVar
 
-
 class Auth:
-    def require_auth(self, path:str, executed_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Determines if authentication is needed.
+        Check if authentication is needed.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
