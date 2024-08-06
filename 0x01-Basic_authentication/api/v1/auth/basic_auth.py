8440bb9b20f73dedb1d347ffc3f0e85a69fd40d5
MachariaP <walburphinehas78@gmail.com>
2024-08-06 13:00:23 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,21 +2,23 @@
 """Basic auth.
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """BasicAuth class that inherits from Auth.
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for Basic Authentication
-        
+        Extracts the Base64 part of the Authorization header for Basic Auth
+
         Args:
             authorization_header (str): The Authorization header.
 
         Returns:
-            str: The Base64 part of the Authorization header, or None if invalid.
+            str: The Base64 part of the Auth header, or None if invalid.
         """
         if authorization_header is None:
             return None
@@ -25,3 +27,24 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 part of the Authorization header.
+
+        Args:
+            base64_authorization_header (str): The Base64 encoded Auth header.
+
+        Return:
+            str: The decoded value as a UTF-8 string, or None if invalid.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
