5ee6fb127d618b485994f605ce687e8899518197
MachariaP <walburphinehas78@gmail.com>
2024-08-06 12:01:09 +0300

    a class BasicAuth that inherits from Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,8 +18,11 @@ auth = None
 AUTH_TYPE = os.getenv('AUTH_TYPE')
 
 # Load the appropriate authentication class based on the environmental variables
-if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "auth":
+    from api.vi.auth.auth import Auth
     auth = Auth()
 
 
