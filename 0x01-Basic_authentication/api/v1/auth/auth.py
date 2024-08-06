5638d27b5b4ca320c88e2b79acfdff967edb8152
MachariaP <walburphinehas78@gmail.com>
2024-08-06 10:48:45 +0300

    a class to manage the API authentication.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,31 +7,13 @@ from typing import List, TypeVar
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        Check if authentication is needed.
-        """
-        if path is None:
-            return True
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        if not path.endswith('/'):
-            path += '/'
-        if path in excluded_paths:
-            return False
-        return True
+        """ Determines if authentication is required """
+        return False
 
     def authorization_header(self, request=None) -> str:
-        """
-        Get the authorization header.
-        """
-        if request is None:
-            return None
-        if 'Authorization' not in request.headers:
-            return None
-        return request.headers.get('Authorization')
+        """ Returns the authorization header from the request """
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Get the current user.
-        """
+        """ Returns the current user """
         return None
