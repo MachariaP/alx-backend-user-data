ad7f6a32c1ad86526db733d943dd6fdf19d0c4ce
MachariaP <walburphinehas78@gmail.com>
2024-08-08 09:28:58 +0300

    Fix: Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,22 +31,23 @@ elif AUTH_TYPE == "session_auth":
 
 @app.before_request
 def before_request():
-    """ Filter each requests before it is processed.
+    """ Filter each request before it is processed.
     """
     if auth is None:
         return
 
     excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/'
-            ]
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     user = auth.current_user(request)
