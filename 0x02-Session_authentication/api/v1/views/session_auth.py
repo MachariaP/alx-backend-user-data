55c625fcb20f49d102d99e972dd328f339a0e8bd
MachariaP <walburphinehas78@gmail.com>
2024-08-12 11:46:30 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python3
-""" Module for Session Authentication Views
-"""
-from flask import Blueprint, request, jsonify
-from models.user import User
+
+"""This module defines routes for session authentication."""
 import os
 
-session_auth = Blueprint('session_auth', __name__)
+from flask import abort, jsonify, request
 
-@session_auth.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+from api.v1.views import app_views
+from models.user import User as DBUser
+from typing import Union
+from flask import session
+
+
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def login():
-    """Handles the login route for session authentication"""
-    from api.v1.app import auth  # Import here to avoid circular import
-
-    email = request.form.get('email')
-    password = request.form.get('password')
-
+    """Login the user and set the session cookie."""
+    email = request.form.get("email")
     if not email:
         return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get("password")
     if not password:
         return jsonify({"error": "password missing"}), 400
 
-    users = User.search({'email': email})
-    if not users or len(users) == 0:
+    try:
+        user: DBUser = DBUser.search({"email": email})[0]
+    except IndexError:
         return jsonify({"error": "no user found for this email"}), 404
 
-    user = users[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
-    session_id = auth.create_session(user.id)
-    response = jsonify(user.to_json())
-    session_name = os.getenv('SESSION_NAME')
-    response.set_cookie(session_name, session_id)
+    from api.v1.app import auth
 
-    return response
+    session_id = auth.create_session(user.id)
+
+    data = jsonify(user.to_json())
+    data.set_cookie(os.environ.get("SESSION_NAME"), session_id)
+
+    return data
