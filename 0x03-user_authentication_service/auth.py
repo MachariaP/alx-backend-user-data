1d89ab14dfa47fa5d2865748670d5be69b9df5e9
MachariaP <walburphinehas78@gmail.com>
2024-08-13 13:02:30 +0300

    a _hash_password method that takes in a password string arguments and returns bytes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""A Hash password method.
+""" Auth module.
 """
 
 import bcrypt
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password using bcrypt.
@@ -14,5 +15,5 @@ def _hash_password(password: str) -> bytes:
         bytes: The salted hash of the password.
     """
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password=password.encode(), salt)
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
