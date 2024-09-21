2879711d0233a1442cd4bb21aa1b98c071e10066
MachariaP <walburphinehas78@gmail.com>
2024-09-21 11:07:02 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python3
-
-"""This module defines routes for session authentication."""
-import os
-
-from flask import abort, jsonify, request
-
+""" Module of Session Authentication
+"""
 from api.v1.views import app_views
-from models.user import User as DBUser
-from typing import Union
-from flask import session
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
 
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
+    """ POST /api/v1/auth_session/login
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+    """
+    user_email = request.form.get('email')
+    user_pswd = request.form.get('password')
 
-@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
-def login():
-    """Login the user and set the session cookie."""
-    email = request.form.get("email")
-    if not email:
+    if not user_email:
         return jsonify({"error": "email missing"}), 400
-
-    password = request.form.get("password")
-    if not password:
+    if not user_pswd:
         return jsonify({"error": "password missing"}), 400
 
     try:
-        user: DBUser = DBUser.search({"email": email})[0]
-    except IndexError:
+        search_users = User.search({'email': user_email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not search_users:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if not user.is_valid_password(password):
+    user = search_users[0]
+    if not user.is_valid_password(user_pswd):
         return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
-
+    session_cookie = getenv("SESSION_NAME")
     session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(session_cookie, session_id)
 
-    data = jsonify(user.to_json())
-    data.set_cookie(os.environ.get("SESSION_NAME"), session_id)
-
-    return data
+    return response
